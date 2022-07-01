bc3d2a95f7dcdd9118f6b6bf4d73ea4b19c8a07a
fromant <fromant65@gmail.com>
2022-07-01 19:25:53 -0300

    Agregados prototipos de funciones en bienes.js

--- Javascript/bienes.js
+++ Javascript/bienes.js
@@ -2,4 +2,23 @@ export class Bienes{
     constructor(id){
         this.id=id;
     }
+
+    determinarOferta(){
+
+    }//Funcion que determina la oferta global del bien
+    //a partir de la suma de la producción de cada empresa
+
+    determinarDemanda(){
+
+    }//Funcion que determina la demanda global del bien
+    //a partir de la suma de demandas de empresas y Pops
+
+    determinarPrecio(){
+        if(this.demanda>this.oferta){
+            this.precio*=1.005;
+        }else if(this.demanda<this.oferta){
+            this.precio/=1.005;
+        }
+    }//Funcion que hace variar el precio en caso de que la oferta o la demandda no sean iguales
 } 
+
